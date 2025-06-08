2d0b20646c6a786650100dda9d7c948edd89a242
sjovanaa <sjovanaaa28@gmail.com>
2025-06-08 11:52:11 +0200

    finalno
    vrata i kljucevi: logika, animacije, izgled, nema zvuk

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,16 +1,26 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour {
     public string requiredKeyColor; // Primer: "Yellow"
     public static List<Door> allDoors = new List<Door>();
+    private Animator animator;
 
     private void Awake() {
+        animator = GetComponent<Animator>();
+
         allDoors.Add(this); // Vrata se registruju automatski
     }
 
     public void Open() {
         Debug.Log("Otvaram vrata: " + gameObject.name);
-        Destroy(gameObject); // ili animacija umesto Destroy
+        animator.SetTrigger("OpenDoor");
+        StartCoroutine(DestroyAfterAnimation());
+    }
+
+    private System.Collections.IEnumerator DestroyAfterAnimation() {
+        // Čekaj dok animacija traje (npr. 2 sekunde, koliko ti traje animacija)
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }
